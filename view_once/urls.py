aab731a4858114324a1be1a5c249e656b870c808
chistev <stephenowabie@gmail.com>
2025-06-19 10:32:33 +0100

    added base template; features page

--- view_once/urls.py
+++ view_once/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('features/', views.features, name='features'),
 ]
--- view_once/views.py
+++ view_once/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'view_once/index.html')
+
+def features(request):
+    return render(request, 'view_once/features.html')
